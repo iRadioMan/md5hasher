6165ed5ed8e8b76019b057478028d01b2267ec5a
RadioMan <ilyadud@mail.ru>
2016-11-05 10:12:33 +0700

    Initial commit

---
+++ MD5_hasher/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace MD5_hasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.getHashesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comparingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.browsePreviousHashFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createAnUpdateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(637, 358);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(637, 358);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.comparingToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(661, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.getHashesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.fileToolStripMenuItem.Text = "Actions";
+            // 
+            // getHashesToolStripMenuItem
+            // 
+            this.getHashesToolStripMenuItem.Name = "getHashesToolStripMenuItem";
+            this.getHashesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.getHashesToolStripMenuItem.Text = "Get hashes";
+            this.getHashesToolStripMenuItem.Click += new System.EventHandler(this.getHashesToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // comparingToolStripMenuItem
+            // 
+            this.comparingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.browsePreviousHashFileToolStripMenuItem,
+            this.compareToolStripMenuItem,
+            this.createAnUpdateToolStripMenuItem});
+            this.comparingToolStripMenuItem.Name = "comparingToolStripMenuItem";
+            this.comparingToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.comparingToolStripMenuItem.Text = "Comparison";
+            // 
+            // browsePreviousHashFileToolStripMenuItem
+            // 
+            this.browsePreviousHashFileToolStripMenuItem.Name = "browsePreviousHashFileToolStripMenuItem";
+            this.browsePreviousHashFileToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.browsePreviousHashFileToolStripMenuItem.Text = "Browse previous hash file";
+            this.browsePreviousHashFileToolStripMenuItem.Click += new System.EventHandler(this.browsePreviousHashFileToolStripMenuItem_Click);
+            // 
+            // compareToolStripMenuItem
+            // 
+            this.compareToolStripMenuItem.Name = "compareToolStripMenuItem";
+            this.compareToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.compareToolStripMenuItem.Text = "Compare";
+            this.compareToolStripMenuItem.Click += new System.EventHandler(this.compareToolStripMenuItem_Click);
+            // 
+            // createAnUpdateToolStripMenuItem
+            // 
+            this.createAnUpdateToolStripMenuItem.Name = "createAnUpdateToolStripMenuItem";
+            this.createAnUpdateToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.createAnUpdateToolStripMenuItem.Text = "Create an update";
+            this.createAnUpdateToolStripMenuItem.Click += new System.EventHandler(this.createAnUpdateToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 397);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.Text = "NADevs MD5 hasher";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem getHashesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comparingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem browsePreviousHashFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createAnUpdateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MD5_hasher/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Threading;
+
+namespace MD5_hasher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string substr = Application.StartupPath + @"\";
+
+        private string ComputeMD5Checksum(string path)
+        {
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, (int)fs.Length);
+                return BitConverter.ToString(md5.ComputeHash(fileData)).Replace("-", String.Empty);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            foreach (string mfile in Directory.GetFiles(Application.StartupPath, "*.*", SearchOption.AllDirectories))
+            {
+                string lfile = mfile;
+                int n = lfile.IndexOf(substr);
+                lfile = lfile.Remove(n, substr.Length);
+                UpdateTextBox(lfile + ";" + ComputeMD5Checksum(mfile) + "\n");
+            }
+        }
+
+        public void UpdateTextBox(string ab)
+        {
+            if (InvokeRequired)
+                Invoke(new Action<string>((s) => richTextBox1.Text = richTextBox1.Text + s), ab);
+            else
+                richTextBox1.Text = richTextBox1.Text + ab;
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        SaveFileDialog sfd = new SaveFileDialog();
+        string f1;
+        bool isfileloaded = false;
+
+        private void DeleteLine(int a_line, RichTextBox textBox)
+        {
+            int start_index = textBox.GetFirstCharIndexFromLine(a_line);
+            int count = textBox.Lines[a_line].Length;
+
+            if (a_line < textBox.Lines.Length - 1)
+            {
+                count += textBox.GetFirstCharIndexFromLine(a_line + 1) -
+                    ((start_index + count - 1) + 1);
+            }
+
+            textBox.Text = textBox.Text.Remove(start_index, count);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter ad = new StreamWriter(sfd.FileName);
+                ad.Write(richTextBox1.Text);
+                ad.Close();
+                MessageBox.Show("Done!");
+            }
+        }
+
+        private void getHashesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void browsePreviousHashFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader fr1 = new StreamReader(ofd.FileName);
+                f1 = fr1.ReadToEnd();
+                fr1.Close();
+                isfileloaded = true;
+            }
+        }
+
+        private void compareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isfileloaded == false)
+            {
+                MessageBox.Show("Please, browse the previous hash file to compare!");
+            }
+            else
+            {
+                richTextBox2.Text = f1;
+                for (int i = 0; i < richTextBox1.Lines.Length; i++)
+                {
+                    for (int a = 0; a < richTextBox2.Lines.Length; a++)
+                    {
+                        if (richTextBox2.Lines[a] == richTextBox1.Lines[i])
+                        {
+                            DeleteLine(i, richTextBox1);
+                            DeleteLine(a, richTextBox2);
+                            a--;
+                        }
+                        else
+                        {
+                            string s = null;
+                            char[] ac = richTextBox1.Lines[i].ToCharArray();
+                            for (int b = 0; b < ac.Length; b++)
+                            {
+                                if (ac[b] == ';')
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    s = s + Convert.ToString(ac[b]);
+                                }
+                            }
+                            if (richTextBox2.Lines[a].StartsWith(s))
+                            {
+                                DeleteLine(a, richTextBox2);
+                                a--;
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("Done!");
+            }
+        }
+
+        private void createAnUpdateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(@"Patch"))
+            {
+                DialogResult ac = MessageBox.Show("The directory Patch exists. Do you want to delete it?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (ac == DialogResult.Yes)
+                {
+                    Directory.Delete(@"Patch");
+                }
+                else
+                {
+                    return;
+                }
+            }
+            Directory.CreateDirectory(@"Patch");
+            string file = "";
+            char[] ab;
+            for (int i = 0; i < richTextBox1.Lines.Length; i++)
+            {
+                ab = richTextBox1.Lines[i].ToCharArray();
+                for (int a = 0; a < ab.Length; a++)
+                {
+                    if (ab[a] == ';')
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(@"Patch\" + file));
+                        File.Copy(substr + file, @"Patch\" + file, true);
+                        file = "";
+                        Array.Clear(ab, 0, ab.Length);
+                        break;
+                    }
+                    else
+                    {
+                        file = file + ab[a];
+                    }
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("NADevs MD5 Hasher alpha\nC0D3D by RadioMan\nhttp://ilyadud.ru\nhttp://vk.com/ilyadud\nEnjoy my soft :P", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            fileToolStripMenuItem.Enabled = true;
+            comparingToolStripMenuItem.Enabled = true;
+        }
+    }
+}
